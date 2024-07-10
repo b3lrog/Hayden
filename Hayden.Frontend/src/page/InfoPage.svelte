1d30eddf2e5ca094ffa3bf5618c99683665b23fc
Balrog <balrog@goatse.email>
2024-07-10 17:33:26 +0100

    Update soyjak wiki url

--- Hayden.Frontend/src/page/InfoPage.svelte
+++ Hayden.Frontend/src/page/InfoPage.svelte
@@ -6,7 +6,7 @@
 <h2>Info</h2>
 <p>{Utility.infoObject.siteName} is an unofficial archive of <a href="https://soyjak.party">soyjak.party</a>.</p>
 <p>This archive is a continuation of <a href="https://chud.site">SudeArchive</a>, all the posts from that archive were copied to this archive and the patched version of Hayden which this archive runs on was written by them.</p>
-<p>In addition to that, this site also includes over 2.3 million posts (without images) from the former <a href="https://soyjak.wiki/Log_Warehouse">Log Warehouse</a>.</p>
+<p>In addition to that, this site also includes over 2.3 million posts (without images) from the former <a href="https://soyjakwiki.net/Log_Warehouse">Log Warehouse</a>.</p>
 <h2>Contact</h2>
 <p>Email: <a href="mailto:balrog@goatse.email"><code>balrog@goatse.email</code></a>.</p>
 </div>
